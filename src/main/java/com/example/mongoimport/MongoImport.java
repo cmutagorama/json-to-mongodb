492d62c5d0957124b3416a1cc94e48fcd6cd0ccc
Charles Mutagorama <charles.mutagorama@risa.gov>
2022-03-10 16:19:47 +0200

    first commit

---
+++ src/main/java/com/example/mongoimport/MongoImport.java
@@ -0,0 +1,56 @@
+package com.example.mongoimport;
+
+import com.mongodb.MongoWriteException;
+import com.mongodb.bulk.BulkWriteResult;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.BulkWriteOptions;
+import com.mongodb.client.model.InsertOneModel;
+import org.bson.Document;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoImport {
+    public static void main(String[] args) throws IOException {
+        com.mongodb.client.MongoClient client = MongoClients.create("mongodb://root:root@localhost:27017");
+
+        MongoDatabase db = client.getDatabase("tweets");
+        MongoCollection<org.bson.Document> coll = db.getCollection("tweets");
+
+        try {
+            // drop previous import
+            coll.drop();
+
+            // bulk approach
+            int count = 0;
+            int batch = 0;
+            List<InsertOneModel<Document>> docs = new ArrayList<>();
+
+            try (BufferedReader br = new BufferedReader(new FileReader("resources/query2_ref.json"))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    docs.add(new InsertOneModel<>(Document.parse(line)));
+                    count ++;
+                    if (count == batch) {
+                        coll.bulkWrite(docs, new BulkWriteOptions().ordered(false));
+                        docs.clear();
+                        count = 0;
+                    }
+                }
+            }
+
+            if (count > 0) {
+                BulkWriteResult bulkWriteResult = coll.bulkWrite(docs, new BulkWriteOptions().ordered(false));
+                System.out.println("Inserted" + bulkWriteResult);
+            }
+
+        } catch (MongoWriteException e) {
+            System.out.println("Error");
+        }
+    }
+}
